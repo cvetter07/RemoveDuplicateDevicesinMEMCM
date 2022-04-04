07a0e6cd607d35c0429d3643d8d8926419643d41
Chris Vetter <41964247+cvetter07@users.noreply.github.com>
2022-04-04 19:16:00 -0400

    Add files via upload

---
+++ RemoveDuplicateDevicesinMEMCM.ps1
@@ -0,0 +1,51 @@
+﻿<#
+##################################################################################################################
+#
+# Microsoft Premier Field Engineering
+# Christopher.Vetter@microsoft.com
+# RemoveDuplicateDevicesinMEMCM.ps1
+# v1.0 Initial creation 4/10/2020 - Removes Devices from a Named Collection.
+#
+# Must run this script from a machine with the Configuration Manager Console installed.
+#
+# YOU MUST INSERT YOUR SITE INFORMATION INTO THE VARIABLES FOR THE SCRIPT TO RUN SUCCESSFULLY!!!!
+# 
+# Microsoft Disclaimer for custom scripts
+# ================================================================================================================
+# The sample scripts are not supported under any Microsoft standard support program or service. The sample scripts
+# are provided AS IS without warranty of any kind. Microsoft further disclaims all implied warranties including, 
+# without limitation, any implied warranties of merchantability or of fitness for a particular purpose. The entire
+# risk arising out of the use or performance of the sample scripts and documentation remains with you. In no event
+# shall Microsoft, its authors, or anyone else involved in the creation, production, or delivery of the scripts be
+# liable for any damages whatsoever (including, without limitation, damages for loss of business profits, business
+# interruption, loss of business information, or other pecuniary loss) arising out of the use of or inability to 
+# use the sample scripts or documentation, even if Microsoft has been advised of the possibility of such damages.
+# ================================================================================================================
+#>
+
+# Site configuration
+$SiteCode = "<Site_Code>" # Site code 
+$ProviderMachineName = "<Server.FQDN>" # SMS Provider machine name
+
+# Customizations
+$initParams = @{}
+#$initParams.Add("Verbose", $true) # Uncomment this line to enable verbose logging
+#$initParams.Add("ErrorAction", "Stop") # Uncomment this line to stop the script on any errors
+
+# Do not change anything below this line
+
+# Import the ConfigurationManager.psd1 module 
+if((Get-Module ConfigurationManager) -eq $null) {
+    Import-Module "$($ENV:SMS_ADMIN_UI_PATH)\..\ConfigurationManager.psd1" @initParams 
+}
+
+# Connect to the site's drive if it is not already present
+if((Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue) -eq $null) {
+    New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $ProviderMachineName @initParams
+}
+
+# Set the current location to be the site code.
+Set-Location "$($SiteCode):\" @initParams
+
+#Remove Devices from Collection
+Get-CMCollectionMember -CollectionName "<Collection Name Here>" | Remove-CMDevice -Force
